e91e146adc3c3c2600ecc750616d345cb59b18da
Gitekkaad <129181365+Gitekkaad@users.noreply.github.com>
2024-08-26 01:22:32 +0200

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Spcap5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start capture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop capture";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Cyan;
+            this.textBox1.Location = new System.Drawing.Point(12, 89);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1825, 327);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.WordWrap = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1479, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Cyan;
+            this.textBox2.Location = new System.Drawing.Point(12, 433);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(1825, 324);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(1850, 789);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Wire Shärkchen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using PacketDotNet;
+using SharpPcap;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Spcap5
+{
+    public partial class Form1 : Form
+    {
+        private ICaptureDevice device;
+        private string efr;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            device = CaptureDeviceList.Instance[0];
+
+            textBox1.Text = "";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            var ver = Pcap.SharpPcapVersion;
+
+            textBox1.Text = "Sharp Pcap Version  " + ver.ToString();
+
+            int readTimeoutMilliseconds = 1000;
+
+            var device = CaptureDeviceList.Instance[0];
+
+            device.OnPacketArrival += new PacketArrivalEventHandler(device_OnPacketArrival);
+
+            device.Open(DeviceModes.Promiscuous, readTimeoutMilliseconds);
+
+            device.StartCapture();
+
+        }
+
+
+        private bool isProcessingPacket = false;
+
+        private void device_OnPacketArrival(object sender, PacketCapture e)
+        {
+
+            if (isProcessingPacket)
+            {
+                return;
+            }
+
+
+            isProcessingPacket = true;
+
+            try
+            {
+                var rawPacket = e.GetPacket();
+                var p = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    textBox1.AppendText(p.ToString() + Environment.NewLine);
+                    textBox1.Update();
+
+
+                    StringBuilder filteredText = new StringBuilder();
+
+                    foreach (byte byteValue in rawPacket.Data)
+                    {
+                        char character = (char)byteValue;
+                        // Fügen Sie nur ASCII-Zeichen hinzu oder ersetzen Sie nicht-ASCII-Zeichen durch "_"
+                        if (character >= 32 && character <= 126)
+                        {
+                            filteredText.Append(character);
+                        }
+                        else
+                        {
+                            filteredText.Append('_');
+                        }
+                    }
+
+                    textBox2.AppendText(filteredText.ToString() + Environment.NewLine);
+                    textBox2.Update();
+
+                });
+            }
+
+            finally
+
+            {
+                isProcessingPacket = false;
+            }
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            if (device != null && device.Started)
+
+            {
+                device.StopCapture();
+
+                MessageBox.Show(device.Statistics.ToString());
+
+                device.Close();
+            }
+
+
+            //Application.Exit();
+        }
+    }
+}
